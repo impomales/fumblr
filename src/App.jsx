5518af9e68039b5acf6a783f6473c928f0b7154d
Isaias <impomales@gmail.com>
2017-11-19 21:08:54 -0500

    add general html layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,45 @@
-const app = <h1>Hello Fumblr.</h1>
 const node = document.getElementById('app');
 
-ReactDOM.render(app, node);
+const NavBar = (props) => (
+	<div>
+		<ul>
+			<li><h1>F</h1></li>
+			<li><input type="text" name="search" placeholder="Search..." /></li>
+			<li>Dashboard</li>
+			<li>Explore</li>
+			<li>Messages</li>
+			<li>Chat</li>
+			<li>Activity</li>
+			<li>Account</li>
+			<li>Make a post</li>
+		</ul>
+	</div>
+);
+
+const AddPost = (props) => (
+	<div>
+		<form>
+			<input type="text" name="title" placeholder="Title" />
+		{/* this will vary depending on post type */}
+			<input type="text" name="data" placeholder="Your text here..." />
+			<input type="text" name="tags" placeholder="#tags" />
+			<hr />
+			<button>Close</button>
+			<button>Post</button>
+		</form>
+	</div>
+);
+
+const PostList = (props) => (
+	<div>List of posts will go here.</div>
+);
+
+const App = (props) => (
+	<div>
+		<NavBar />
+		<AddPost />
+		<PostList />
+	</div>
+);
+
+ReactDOM.render(<App />, node);
--- static/App.js
+++ static/App.js
@@ -1,10 +1,108 @@
-'use strict';
+"use strict";
 
-var app = React.createElement(
-  'h1',
-  null,
-  'Hello Fumblr.'
-);
 var node = document.getElementById('app');
 
-ReactDOM.render(app, node);
+var NavBar = function NavBar(props) {
+	return React.createElement(
+		"div",
+		null,
+		React.createElement(
+			"ul",
+			null,
+			React.createElement(
+				"li",
+				null,
+				React.createElement(
+					"h1",
+					null,
+					"F"
+				)
+			),
+			React.createElement(
+				"li",
+				null,
+				React.createElement("input", { type: "text", name: "search", placeholder: "Search..." })
+			),
+			React.createElement(
+				"li",
+				null,
+				"Dashboard"
+			),
+			React.createElement(
+				"li",
+				null,
+				"Explore"
+			),
+			React.createElement(
+				"li",
+				null,
+				"Messages"
+			),
+			React.createElement(
+				"li",
+				null,
+				"Chat"
+			),
+			React.createElement(
+				"li",
+				null,
+				"Activity"
+			),
+			React.createElement(
+				"li",
+				null,
+				"Account"
+			),
+			React.createElement(
+				"li",
+				null,
+				"Make a post"
+			)
+		)
+	);
+};
+
+var AddPost = function AddPost(props) {
+	return React.createElement(
+		"div",
+		null,
+		React.createElement(
+			"form",
+			null,
+			React.createElement("input", { type: "text", name: "title", placeholder: "Title" }),
+			React.createElement("input", { type: "text", name: "data", placeholder: "Your text here..." }),
+			React.createElement("input", { type: "text", name: "tags", placeholder: "#tags" }),
+			React.createElement("hr", null),
+			React.createElement(
+				"button",
+				null,
+				"Close"
+			),
+			React.createElement(
+				"button",
+				null,
+				"Post"
+			)
+		)
+	);
+};
+
+var PostList = function PostList(props) {
+	return React.createElement(
+		"div",
+		null,
+		"List of posts will go here."
+	);
+};
+
+var App = function App(props) {
+	return React.createElement(
+		"div",
+		null,
+		React.createElement(NavBar, null),
+		React.createElement(AddPost, null),
+		React.createElement(PostList, null)
+	);
+};
+
+ReactDOM.render(React.createElement(App, null), node);

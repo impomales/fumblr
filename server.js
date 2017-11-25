66a939f16145f44f25068424b17258f891fb1d40
Isaias <impomales@gmail.com>
2017-11-25 15:32:12 -0500

    add post route for adding new post

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 const posts = [
@@ -6,6 +7,7 @@ const posts = [
 		id: 1,
 		title: "First Post",
 		data: "blah blah blah",
+		tags: ["#first"],
 		posted_by: "impomales",
 		notes: 23
 	},
@@ -14,18 +16,21 @@ const posts = [
 		title: "Whatever",
 		data: "...",
 		posted_by: "nycafana",
+		tags: ["#Afana", "#NYC"],
 		notes: 100
 	},
 	{
 		id: 3,
 		title: "Happy Thanksgiving",
 		data: "Turkey DAY YAAAASS!!!!",
+		tags: ["#Thanksgiving", "#Isaias"],
 		posted_by: "impomales",
 		notes: 500
 	}
 ];
 
 app.use(express.static('static'));
+app.use(bodyParser.json());
 
 app.get('/api/posts', (req, res) => {
 	const metadata = { count: posts.length };
@@ -42,6 +47,17 @@ app.get('/api/posts/:id', (req, res) => {
 		});
 		return;
 	}
-})
+});
+
+app.post('/api/posts', (req, res) => {
+	const newPost = req.body;
+
+	newPost.id = posts.length + 1;
+	newPost.posted_by = 'impomales';
+	newPost.notes = 0;
+
+	posts.push(newPost);
+	res.json(newPost);
+});
 
 app.listen(process.env.PORT || 8080, () => console.log('App is listening on port 8080'));
